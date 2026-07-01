dc3468864462fd1647db4fba4ca5d465fc9294e7
Brody <brody@users.noreply.github.com>
2026-07-02 06:23:31 +0800

    ⚙️ Add GitHub Pages base path

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/clipai-frontend/',
   plugins: [react()],
   server: {
     port: 3000,
